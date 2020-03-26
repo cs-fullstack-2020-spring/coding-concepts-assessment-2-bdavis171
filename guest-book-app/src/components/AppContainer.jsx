a5d613e4a28664596cca8689b9ce9cf0aeba4f96
bdavis171 <bdavis171@gmail.com>
2020-03-26 09:22:33 -0500

    created app, add 4 components to components folder

---
+++ guest-book-app/src/components/AppContainer.jsx
@@ -0,0 +1,18 @@
+import React, { Component, Fragment } from 'react';
+
+
+class AppContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <Fragment>
+                <h1>Guest Book App</h1>
+            </Fragment>
+         );
+    }
+}
+ 
+export default AppContainer;
